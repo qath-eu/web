26b84e64807e379ab2365f8080d09121b7a21ade
Rutger van Rees <rutger@vanrees.nl>
2026-09-22 13:40:12 +0200

    Boilerplate code from create-next-app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center text-center bg-black text-white p-4">
+      <h1 className="text-4xl font-semibold">
+        European Transport Intelligence
+      </h1>
+    </div>
+  );
+}
